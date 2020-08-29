f33afa9a4b63790161093ad9d7c484775d5208fc
urtbest86 <urtbest86@gmail.com>
2020-08-29 21:18:05 +0900

    CCTV 메뉴 삽입 -각 버튼에 맞는 영상 호출

---
+++ app/src/main/java/com/example/harboreye/LiveVideoActivity.java
@@ -0,0 +1,124 @@
+package com.example.harboreye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.harboreye.ui.CCTV.CCTVFragment;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class LiveVideoActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private PlayerControlView playerControlView;
+    private SimpleExoPlayer player;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //가로 화면 유지
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        setContentView(R.layout.activity_live_video);
+
+        playerView = findViewById(R.id.video_view);
+
+        initializePlayer();
+    }
+
+    //엑소플레이어 빌드
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+
+        playerView.setPlayer(player);
+        Intent intent = getIntent();
+        Uri uri = Uri.parse(intent.getExtras().getString("url"));
+        MediaSource mediaSource = buildMediaSource(uri);
+
+
+        player.prepare(mediaSource, false, false);
+        player.setPlayWhenReady(playWhenReady);
+
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this,getString(R.string.app_name));
+        return new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(uri);
+    }
+
+    //시작
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT >= 24) {
+            initializePlayer();
+        }
+    }
+
+    //시작
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT < 24 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    //시작 ui
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    //일시정지
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT < 24) {
+            releasePlayer();
+        }
+    }
+
+    //정지
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT >= 24) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playWhenReady = player.getPlayWhenReady();
+            currentWindow = player.getCurrentWindowIndex();
+            player.release();
+            player = null;
+        }
+    }
+}
+
--- app/src/main/java/com/example/harboreye/ui/CCTV/CCTVFragment.java
+++ app/src/main/java/com/example/harboreye/ui/CCTV/CCTVFragment.java
@@ -1,10 +1,12 @@
 package com.example.harboreye.ui.CCTV;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,27 +15,42 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.harboreye.LiveVideoActivity;
 import com.example.harboreye.R;
 
-public class CCTVFragment extends Fragment {
+public class CCTVFragment extends Fragment implements View.OnClickListener {
 
-    private CCTVViewModel CCTVViewModel;
-
+    private Button button1;
+    private Button button2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //가로 화면 유지
 
-        CCTVViewModel =
-                ViewModelProviders.of(this).get(CCTVViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_cctv, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_cctv);
-        CCTVViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+       View root = inflater.inflate(R.layout.fragment_cctv, container, false);
+       button1=root.findViewById(R.id.button1);
+       button1.setOnClickListener(this);
+
+        button2=root.findViewById(R.id.button2);
+        button2.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(getContext(), LiveVideoActivity.class);
+        String selecturl = null;
+        switch (v.getId()){
+            case R.id.button1:
+                selecturl=getString(R.string.button1_url);
+                break;
+            case R.id.button2:
+                selecturl=getString(R.string.button2_url);
+                break;
+            default:
+               break;
+        }
+        intent.putExtra("url",selecturl);
+        startActivity(intent);
     }
 }
